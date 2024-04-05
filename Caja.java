92f519ff5160448f3f28986656970619a0992b32
juli <julian.gamez@escuelaestech.es>
2024-04-05 10:06:36 +0200

    cambio julian

--- Caja.java
+++ Caja.java
@@ -1,3 +1,10 @@
 public class Caja {
     
+    double alto , ancho, largo;
+
+    public Caja (double altura, double anchura, double largura){
+        alto =altura;
+        ancho = anchura;
+        largo = largura;
+    }
 }
