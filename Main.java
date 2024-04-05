3b595bebc0ae0fe322e750a985d823041f98834a
juli <julian.gamez@escuelaestech.es>
2024-04-05 10:18:10 +0200

    relov5

--- Main.java
+++ Main.java
@@ -3,13 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+<<<<<<< HEAD
 
+=======
+>>>>>>> 9006afd (main modificado)
         
         Caja caja1 = new Caja(3, 4,3);
         Caja caja2 = new Caja(4, 10,5);
         caja2.alto = 100;
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 9006afd (main modificado)
         Coche coche1 = new Coche("Opel", "Corsa", "Azul", 0, 150);
 
         String marca = sc.nextLine();
@@ -25,6 +31,10 @@ public class Main {
         }
         sc.close();
     }
+<<<<<<< HEAD
 
 }
 
+=======
+}
+>>>>>>> 9006afd (main modificado)
