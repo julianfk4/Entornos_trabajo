a690fcc2304050428b7cf62b270080be7d448451
juanmartinezgarcia <juanmartinezg2004@gmail.com>
2024-04-05 09:57:50 +0200

    Parte Juan terminada

--- Coche.java
+++ Coche.java
@@ -1,3 +1,14 @@
 public class Coche {
-    
+    // atributos
+    String marca, modelo, color;
+    double velocidad, velocidadMax;
+ 
+    // metodo constructor (Tiene que ir con el nombre de la clase)
+    public Coche (String marca, String modelo, String color, double velocidad, double velocidadMax) {
+        marca= marca;
+        modelo = modelo;
+        color=color;
+        velocidad = velocidad;
+        velocidadMax = velocidadMax;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+
+        Coche coche1 = new Coche("Opel", "Corsa", "Azul", 0, 150);
+
+        String marca = sc.nextLine();
+        String modelo = sc.nextLine();
+        String color = sc.nextLine();
+        double velocidad = sc.nextDouble();
+        double velocidadMax = sc.nextDouble();
+
+        Coche cocheNuevo = new Coche(marca, modelo, color, velocidad, velocidadMax);
+
+        if(cocheNuevo.velocidadMax>coche1.velocidadMax){
+            System.out.println("El coche nuevo es mas rapido que el coche 1");
+        }
+        sc.close();
+    }
 }
