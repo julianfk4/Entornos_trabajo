b3469665956df352b72f5133d9a23b95f676ffe7
juli <julian.gamez@escuelaestech.es>
2024-04-05 10:14:30 +0200

    feature de nuevo

--- Caja.java
+++ Caja.java
@@ -1,10 +1,3 @@
 public class Caja {
     
-    double alto , ancho, largo;
-
-    public Caja (double altura, double anchura, double largura){
-        alto =altura;
-        ancho = anchura;
-        largo = largura;
-    }
 }
