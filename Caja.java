fba9d79c52abb8e295c935b48a80a110ea71ebfd
juli <julian.gamez@escuelaestech.es>
2024-04-08 11:04:42 +0200

    cherry pick solving

--- Caja.java
+++ Caja.java
@@ -1,3 +1,19 @@
 public class Caja {
     
+<<<<<<< HEAD
 }
+=======
+    double alto , ancho, largo;
+
+    public Caja (double altura, double anchura, double largura){
+        alto =altura;
+        ancho = anchura;
+        largo = largura;
+    }
+    public void puerta (double alt_puerta, double anc_puerta, double larg_puerta){
+        alto = alt_puerta;
+        ancho = anc_puerta;
+        largo = larg_puerta;
+    }
+}
+>>>>>>> 784139b (comit para el cherry)
