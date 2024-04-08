c3af121afe41858e3566027d0652b271d2c03b4e
juli <julian.gamez@escuelaestech.es>
2024-04-08 12:00:16 +0200

    stash

--- Main.java
+++ Main.java
@@ -10,7 +10,10 @@ public class Main {
         Caja caja1 = new Caja(3, 4,3);
         Caja caja2 = new Caja(4, 10,5);
         caja2.alto = 100;
-
+        caja1.largo =50;
+        
+        double resultado = caja1.largo*caja2.alto;
+        System.out.println(resultado);
 
         Coche coche1 = new Coche("Opel", "Corsa", "Azul", 0, 150);
 
